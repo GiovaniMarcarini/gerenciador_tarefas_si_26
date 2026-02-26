0bfb07a2eeaf251cbc590b67e64dfa25f7bced69
giovani <inf_giovani@hotmail.com>
2026-02-26 20:09:03 -0300

    Aula 26/02 - parte 2

---
+++ lib/widgets/conteudo_form_dialog.dart
@@ -0,0 +1,53 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../model/tarefa.dart';
+
+class ConteudoFormDialog extends StatefulWidget{
+  final Tarefa? tarefaAtual;
+
+  ConteudoFormDialog({Key? key, this.tarefaAtual }):super(key: key);
+
+  ConteudoFormDialogState createState() => ConteudoFormDialogState();
+
+}
+
+class ConteudoFormDialogState extends State<ConteudoFormDialog>{
+  final formkey = GlobalKey<FormState>();
+  final descricaoController = TextEditingController();
+  final prazoController = TextEditingController();
+  final prazoFormat = DateFormat('dd/MM/yyyy');
+
+
+  @override
+  void initState() {
+    super.initState();
+    descricaoController.text = widget.tarefaAtual!.descricao;
+    prazoController.text =widget.tarefaAtual!.prazoFormatado;
+  }
+  
+  @override
+  Widget build (BuildContext context){
+    return Form(
+      key: formkey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: descricaoController,
+              decoration: InputDecoration(labelText: 'Descrição'),
+              validator: (String? valor){
+                if (valor == null || valor.isEmpty){
+                  return 'Informe a Descrição';
+                }
+                return null;
+              }
+            )
+          ],
+        )
+    );
+  }
+}
