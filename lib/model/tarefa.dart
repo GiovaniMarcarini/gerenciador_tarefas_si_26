93ef8e929c4e5d1bf072ba7e67e40c9c99075e17
giovani <inf_giovani@hotmail.com>
2026-03-19 21:18:59 -0300

    Aula 19/03 - parte 1 (provider e model)

--- lib/model/tarefa.dart
+++ lib/model/tarefa.dart
@@ -3,7 +3,7 @@
 import 'package:intl/intl.dart';
 
 class Tarefa{
-
+  static const NOME_TABELA = 'tarefas';
   static const CAMPO_ID = '_id';
   static const CAMPO_DESCRICAO = 'descricao';
   static const CAMPO_PRAZO = 'prazo';
@@ -21,4 +21,16 @@ class Tarefa{
     return DateFormat('dd/MM/yyyy').format(prazo!);
   }
 
+  Map<String, dynamic> toMap() => <String, dynamic>{
+    CAMPO_ID: id,
+    CAMPO_DESCRICAO : descricao,
+    CAMPO_PRAZO : prazo == null ? null : DateFormat("yyyy-MM-dd").format(prazo!)
+  };
+  
+  factory Tarefa.fromMap(Map<String, dynamic> map) => Tarefa(
+      id: map[CAMPO_ID] is int ? map[CAMPO_ID] : null, 
+      descricao: map[CAMPO_DESCRICAO] is String ? map[CAMPO_DESCRICAO] : '',
+      prazo: map[CAMPO_PRAZO] == null ? null :
+          DateFormat("yyyy-MM-dd").parse(map[CAMPO_PRAZO])
+  );
 }
