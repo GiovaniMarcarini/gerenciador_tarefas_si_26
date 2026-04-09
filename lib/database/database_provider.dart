cadf641a9ab14e8d7cd4bb94be267b86313af57c
giovani <inf_giovani@hotmail.com>
2026-04-09 20:09:20 -0300

    Aula 09/04 - parte 3

--- lib/database/database_provider.dart
+++ lib/database/database_provider.dart
@@ -5,7 +5,7 @@ import '../model/tarefa.dart';
 
 class DatabaseProvider{
   static const _dbName = 'cadastro_tarefas.db';
-  static const _dbVersion = 1;
+  static const _dbVersion = 2;
 
   DatabaseProvider.int();
 
@@ -37,13 +37,20 @@ class DatabaseProvider{
       CREATE TABLE ${Tarefa.NOME_TABELA} (
       ${Tarefa.CAMPO_ID} INTEGER PRIMARY KEY AUTOINCREMENT,
       ${Tarefa.CAMPO_DESCRICAO} TEXT NOT NULL,
-      ${Tarefa.CAMPO_PRAZO} TEXT );
+      ${Tarefa.CAMPO_PRAZO} TEXT,
+      ${Tarefa.CAMPO_FINALIZADA} INTEGER NOT NULL DEFAULT 0);
       '''
     );
   }
 
   Future<void>_onUpgrade(Database db, int oldVersion, int newVersion) async {
-
+    switch (oldVersion){
+      case 1:
+        await db.execute('''
+          ALTER TABLE ${Tarefa.NOME_TABELA}
+          ADD ${Tarefa.CAMPO_FINALIZADA} INTEGER NOT NULL DEFAULT 0;
+        ''');
+    }
   }
 
   Future<void> close() async{
